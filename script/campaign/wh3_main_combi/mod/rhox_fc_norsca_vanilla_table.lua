41a58220aa40b7b0076a12fa7261bbe54bd5b941
RhoxGSAINT <swordknight@naver.com>
2024-02-10 23:04:48 +0900

    Tzeentch LHs
    Tzeentch LHs

--- script/campaign/wh3_main_combi/mod/rhox_fc_norsca_vanilla_table.lua
+++ script/campaign/wh3_main_combi/mod/rhox_fc_norsca_vanilla_table.lua
@@ -1,3 +1,23 @@
+
+-----------------------------Vanilla Legendary Hero thing
+------------Baesonling --nedd check
+table.insert(character_unlocking.character_data["aekold"]["override_allowed_factions"]["main_warhammer"], "wh_main_nor_baersonling")
+table.insert(character_unlocking.character_data["aekold"]["mission_chain_keys"]["main_warhammer"], "rhox_baersonling_tze_aekold_helbrass_stage_1_nor")
+table.insert(character_unlocking.character_data["aekold"]["mission_chain_keys"]["main_warhammer"], "rhox_baersonling_tze_aekold_helbrass_stage_2_nor")
+character_unlocking.character_data["aekold"]["mission_keys"]["wh_main_nor_baersonling"]={}
+character_unlocking.character_data["aekold"]["mission_keys"]["wh_main_nor_baersonling"]["main_warhammer"]="rhox_baersonling_tze_aekold_helbrass_stage_1_nor"
+
+table.insert(character_unlocking.character_data["scribes"]["override_allowed_factions"]["main_warhammer"], "wh_main_nor_baersonling")
+table.insert(character_unlocking.character_data["scribes"]["mission_chain_keys"]["main_warhammer"], "rhox_baersonling_tze_blue_scribes_stage_1_nor")
+table.insert(character_unlocking.character_data["scribes"]["mission_chain_keys"]["main_warhammer"], "rhox_baersonling_tze_blue_scribes_stage_2_b_nor")
+table.insert(character_unlocking.character_data["scribes"]["mission_chain_keys"]["main_warhammer"], "rhox_baersonling_tze_blue_scribes_stage_2_nor")
+table.insert(character_unlocking.character_data["scribes"]["mission_chain_keys"]["main_warhammer"], "rhox_baersonling_tze_blue_scribes_stage_3_nor")
+table.insert(character_unlocking.character_data["scribes"]["missions_to_trigger_dilemma"]["main_warhammer"], "rhox_baersonling_tze_blue_scribes_stage_3_nor")
+character_unlocking.character_data["scribes"]["mission_keys"]["wh_main_nor_baersonling"]={}
+character_unlocking.character_data["scribes"]["mission_keys"]["wh_main_nor_baersonling"]["main_warhammer"]="rhox_baersonling_tze_blue_scribes_stage_1_nor"
+
+
+
 cm:add_first_tick_callback(
 	function()
         campaign_traits.legendary_lord_defeated_traits["hkrul_adella"] ="hkrul_defeated_trait_adella"
