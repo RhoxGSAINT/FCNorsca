2a491b35bebf8cbf865c3d907c654ad35349363e
RhoxGSAINT <swordknight@naver.com>
2024-03-05 00:00:01 +0900

    Surtha Ek

--- script/frontend/mod/rhox_fc_norsca_frontend.lua
+++ script/frontend/mod/rhox_fc_norsca_frontend.lua
@@ -36,7 +36,7 @@ core:add_ui_created_callback(
 			mixer_add_starting_unit_list_for_faction("wh_main_nor_aesling", {"wh_dlc08_nor_inf_marauder_champions_1","wh_dlc08_nor_inf_marauder_hunters_0","wh_dlc08_nor_mon_war_mammoth_0","scm_fc_norsca_aesling_nor_marauder_great_weapons"})
 			mixer_add_faction_to_major_faction_list("wh_main_nor_aesling")
 			
-			mixer_change_lord_name("1752931370", "hkrul_hrothgar")
+			mixer_change_lord_name("1752931370", "hkrul_surtha_ek")
 			mixer_add_starting_unit_list_for_faction("wh_main_nor_varg", {"wh_dlc08_nor_inf_marauder_champions_1","wh_dlc08_nor_inf_marauder_hunters_0","wh_dlc08_nor_mon_war_mammoth_0","wh_main_nor_inf_chaos_marauders_1"})
 			mixer_add_faction_to_major_faction_list("wh_main_nor_varg")
 			
