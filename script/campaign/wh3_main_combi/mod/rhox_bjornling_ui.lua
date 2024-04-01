39f1da76af7e5bcc611556f5e27ef24bdc2c896b
RhoxGSAINT <swordknight@naver.com>
2024-04-01 12:13:49 +0900

    Partypus ships
    And fish transfer

--- script/campaign/wh3_main_combi/mod/rhox_bjornling_ui.lua
+++ script/campaign/wh3_main_combi/mod/rhox_bjornling_ui.lua
@@ -10,6 +10,9 @@ cm:add_first_tick_callback(
             local result = core:get_or_create_component("rhox_bjornling_winter_button", "ui/campaign ui/rhox_bjornling_button_frame.twui.xml", parent_ui)
             result:SetContextObject(cco("CcoCampaignFaction", bjornling_faction))
             local result2 = core:get_or_create_component("rhox_bjornling_devotion_holder", "ui/campaign ui/rhox_bjornling_devotion_holder.twui.xml", parent_ui)
+            local result3 = core:get_or_create_component("rhox_bjornling_ritual_button", "ui/campaign ui/rhox_bjornling_fish_ritual_button.twui.xml", parent_ui)
+            result3:SetContextObject(cco("CcoCampaignFaction", bjornling_faction))
+            local result4 = core:get_or_create_component("rhox_bjornling_resource_holder", "ui/campaign ui/rhox_bjornling_fish_resource_holder.twui.xml", parent_ui)
             
         end
     end
