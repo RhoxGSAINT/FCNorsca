5de622de17c08e8c3be4af36ea5ad38ae4a5b625
RhoxGSAINT <swordknight@naver.com>
2024-04-01 16:03:22 +0900

    Fish ancillary
    and Raze blocking

--- script/campaign/wh3_main_combi/mod/rhox_bjornling_ui.lua
+++ script/campaign/wh3_main_combi/mod/rhox_bjornling_ui.lua
@@ -14,6 +14,37 @@ cm:add_first_tick_callback(
             result3:SetContextObject(cco("CcoCampaignFaction", bjornling_faction))
             local result4 = core:get_or_create_component("rhox_bjornling_resource_holder", "ui/campaign ui/rhox_bjornling_fish_resource_holder.twui.xml", parent_ui)
             
+            core:add_listener(
+                "rhox_bjornling_disable_occupation_options",
+                "PanelOpenedCampaign",
+                function(context)
+                    return context.string == "settlement_captured"
+                end,
+                function(context)
+                    local panel = find_uicomponent(core:get_ui_root(), "settlement_captured")
+					if not panel then
+						return
+					end
+                    local kill = find_uicomponent(panel, "1963655228")
+                    local maim = find_uicomponent(panel, "1369123792")
+                    local burn = find_uicomponent(panel, "1824195232")
+                    local shovel = find_uicomponent(panel, "1292694896")
+                            
+                    if kill then 
+                        kill:SetVisible(false)
+                    end
+                    if maim then
+                        maim:SetVisible(false)
+                    end
+                    if burn then
+                        burn:SetVisible(false)
+                    end  
+                    if shovel then
+                        shovel:SetVisible(false)
+                    end  
+                end,
+                true
+            )
         end
     end
 )
