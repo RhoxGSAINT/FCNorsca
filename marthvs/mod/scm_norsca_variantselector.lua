7c30b408c560060fa92592b8f86e5780ae7c3521
RhoxGSAINT <swordknight@naver.com>
2024-04-05 11:08:18 +0900

    variant selector
    variant selector

---
+++ marthvs/mod/scm_norsca_variantselector.lua
@@ -0,0 +1,8 @@
+return {
+["rhox_baersonling_daemon_prince_tzeentch"] = {"rhox_baersonling_daemon_prince_tzeentch_1","rhox_baersonling_daemon_prince_tzeentch_2","rhox_baersonling_daemon_prince_tzeentch_3"},
+["rhox_baersonling_chs_lord_mtze"] = {"rhox_baersonling_chs_lord_mtze_01","rhox_baersonling_chs_lord_mtze_02","rhox_baersonling_chs_lord_mtze_03","rhox_baersonling_chs_lord_mtze_04"},
+["nor_skin_wolf_lord"] = {"nor_skin_wolf_lord","nor_skin_wolf_lord_02","nor_skin_wolf_lord_03"}
+};
+
+
+
