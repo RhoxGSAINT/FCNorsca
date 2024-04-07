f34aaf1c7d3db13c8de256f580d5c6f28617d184
RhoxGSAINT <swordknight@naver.com>
2024-04-07 20:22:44 +0900

    mount and mission
    mount and mission

--- script/campaign/mod/rhox_fc_norsca_mission.lua
+++ script/campaign/mod/rhox_fc_norsca_mission.lua
@@ -77,7 +77,10 @@ core:add_listener(
         local faction = context:faction()
         local x, y = cm:find_valid_spawn_location_for_character_from_character(faction:name(), cm:char_lookup_str(faction:faction_leader()), true, 10)
         if x ~= -1 and y ~= -1 then
-            cm:spawn_agent_at_position(faction, x, y, "wizard", "wh_dlc08_nor_shaman_sorcerer_death")
+            local agent = cm:spawn_agent_at_position(faction, x, y, "wizard", "wh_dlc08_nor_shaman_sorcerer_metal")
+            if agent then
+                cm:replenish_action_points(cm:char_lookup_str(agent))
+            end
         end
     end,
     true
