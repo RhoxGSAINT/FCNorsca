b169e5a0febe286c12cea0e1f10b66a46daf60c9
RhoxGSAINT <swordknight@naver.com>
2024-04-08 22:16:48 +0900

    bug

--- script/campaign/mod/rhox_fc_norsca_mission.lua
+++ script/campaign/mod/rhox_fc_norsca_mission.lua
@@ -78,7 +78,7 @@ core:add_listener(
         local faction = context:faction()
         local x, y = cm:find_valid_spawn_location_for_character_from_character(faction:name(), cm:char_lookup_str(faction:faction_leader()), true, 10)
         if x ~= -1 and y ~= -1 then
-            local character = cm:spawn_agent_at_position(faction, x, y, "wizard", "wh_dlc08_nor_shaman_sorcerer_death")
+            local character = cm:spawn_agent_at_position(faction, x, y, "wizard", "wh_dlc08_nor_shaman_sorcerer_metal")
             if character then
                 cm:replenish_action_points(cm:char_lookup_str(character))
             end
