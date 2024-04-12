20c1fc6f3a720c5f244a61cd9a189afab1248d40
RhoxGSAINT <swordknight@naver.com>
2024-04-13 00:08:37 +0900

    hildr

--- script/campaign/mod/rhox_bjornling_ui.lua
+++ script/campaign/mod/rhox_bjornling_ui.lua
@@ -28,6 +28,12 @@ end
 cm:add_first_tick_callback(
     function()
         if cm:get_local_faction_name(true) == bjornling_faction then
+            mixer_disable_hero_recruitment(bjornling_faction, "wh_dlc08_nor_fimir_balefiend_fire", "dignitary", "", true, true)
+            mixer_disable_hero_recruitment(bjornling_faction, "wh_dlc08_nor_fimir_balefiend_shadow", "dignitary", "", true, true)
+        
+        
+
+        
             local parent_ui = find_uicomponent(core:get_ui_root(), "hud_campaign", "resources_bar_holder", "resources_bar");
             local result = core:get_or_create_component("rhox_bjornling_winter_button", "ui/campaign ui/rhox_bjornling_button_frame.twui.xml", parent_ui)
             result:SetContextObject(cco("CcoCampaignFaction", bjornling_faction))
