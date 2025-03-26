889dc1ee9dee9391483cab86281934cd99ad9b82
RhoxGSAINT <swordknight@naver.com>
2025-03-26 22:50:10 +0900

    6.1

--- script/campaign/mod/rhox_bjornling_ui.lua
+++ script/campaign/mod/rhox_bjornling_ui.lua
@@ -27,17 +27,15 @@ end
 
 cm:add_first_tick_callback(
     function()
-        if cm:get_local_faction_name(true) == bjornling_faction then
-            mixer_disable_hero_recruitment(bjornling_faction, "wh_dlc08_nor_fimir_balefiend_fire", "dignitary", "", true, true)
-            mixer_disable_hero_recruitment(bjornling_faction, "wh_dlc08_nor_fimir_balefiend_shadow", "dignitary", "", true, true)
-        
-        
+        if cm:get_local_faction_name(true) == bjornling_faction then   
 
         
             local parent_ui = find_uicomponent(core:get_ui_root(), "hud_campaign", "resources_bar_holder", "resources_bar");
+            --[[
             local result = core:get_or_create_component("rhox_bjornling_winter_button", "ui/campaign ui/rhox_bjornling_button_frame.twui.xml", parent_ui)
             result:SetContextObject(cco("CcoCampaignFaction", bjornling_faction))
             local result2 = core:get_or_create_component("rhox_bjornling_devotion_holder", "ui/campaign ui/rhox_bjornling_devotion_holder.twui.xml", parent_ui)
+            --]]
             local result3 = core:get_or_create_component("rhox_bjornling_ritual_button", "ui/campaign ui/rhox_bjornling_fish_ritual_button.twui.xml", parent_ui)
             result3:SetContextObject(cco("CcoCampaignFaction", bjornling_faction))
             local result4 = core:get_or_create_component("rhox_bjornling_resource_holder", "ui/campaign ui/rhox_bjornling_fish_resource_holder.twui.xml", parent_ui)
@@ -73,7 +71,7 @@ cm:add_first_tick_callback(
                 end,
                 true
             )
-            
+            --[[
             core:add_listener(
                 "rhox_bjornling_winter_panel",
                 "PanelOpenedCampaign",
@@ -98,7 +96,7 @@ cm:add_first_tick_callback(
                 end,
                 true
             )
-            
+            --]]
             local norsca_gods_frame = find_uicomponent(core:get_ui_root(), "hud_campaign", "resources_bar_holder", "resources_bar", "norsca_favour")
             if norsca_gods_frame then
                 norsca_gods_frame:SetVisible(false)
